bc9ea527052daa3f7877cc56f9730c858ffc346f
LucasBruder <lbruder827@gmail.com>
2015-05-07 22:50:23 -0500

    Working on getting the navigation of the course webpage down

--- src/Downloader.java
+++ src/Downloader.java
@@ -130,6 +130,33 @@ public class Downloader {
 		// Let the page load
 		Thread.sleep(MAX_WAIT);
 
+		// switch to content frame
+		c.switchTo().frame(CONTENT_FRAME);
+		// get all of the windows on the left side
+		List<WebElement> left_side_bar = c.findElements(By
+				.xpath("//*[@id=\"courseMenuPalette_contents\"]/ul/li"));
+
+		System.out.println(classname);
+		System.out.println(left_side_bar.size());
+
+		for (int i = 0; i < left_side_bar.size(); i++) {
+			// do stuff
+			WebElement w = left_side_bar.get(i);
+			if (w.getText().contains("Announcement")) {
+				left_side_bar = c.findElements(By
+						.xpath("//*[@id=\"courseMenuPalette_contents\"]"));
+				Thread.sleep(MAX_WAIT);
+				break;
+			} else {
+				w.click();
+				Thread.sleep(MAX_WAIT);
+			}
+			c.navigate().back();
+			left_side_bar = c.findElements(By
+					.xpath("//*[@id=\"courseMenuPalette_contents\"]"));
+			Thread.sleep(MAX_WAIT);
+		}
+
 		c.close();
 	}
 
